0f0e5e2cd6853edef6c5b5042e198437140e6aef
deweni123456789 <schoford91@gmail.com>
2025-09-10 10:14:07 +0530

    Update video.py

--- modules/video.py
+++ modules/video.py
@@ -1,5 +1,5 @@
 import os, asyncio, urllib.request
-from pyrogram import Client, filters
+from pyrogram import Client, filters, utils
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import DOWNLOAD_PATH, DEVELOPER, SUPPORT_GROUP
 from utils.downloader import download_video
@@ -15,7 +15,6 @@ def register_video(app: Client):
 
     @app.on_message(filters.command("video"))
     async def video_handler(client, message):
-        # Get query from command or reply
         args = message.text.split(maxsplit=1)
         query = args[1] if len(args) > 1 else (
             message.reply_to_message.text if message.reply_to_message else ""
@@ -35,8 +34,11 @@ def register_video(app: Client):
             data["file_path"], data["title"], data["uploader"], data["duration"], data["thumbnail"]
         )
 
-        requester = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
-        caption = f"📹 {title}\nRequested by: {requester}\nSource: {data['webpage_url']}"
+        # Escape for markdown_v2
+        requester_name = utils.escape_markdown(message.from_user.first_name, version=2)
+        title = utils.escape_markdown(title, version=2)
+        performer = utils.escape_markdown(performer, version=2)
+        caption = f"📹 *{title}*\nUploaded by: {performer}\nRequested by: [{requester_name}](tg://user?id={message.from_user.id})\n[Source]({data['webpage_url']})"
 
         me = await client.get_me()
         await msg.edit("⬆️ Uploading video...")
@@ -45,7 +47,7 @@ def register_video(app: Client):
             message.chat.id,
             video=file_path,
             caption=caption,
-            parse_mode="markdown",
+            parse_mode="markdown_v2",
             duration=duration,
             thumb=thumb,
             reply_markup=build_buttons(me.username),
