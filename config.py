7faa7d02358ec32980278bfbb654bc5c4fca38f5
deweni123456789 <schoford91@gmail.com>
2025-09-10 06:38:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", "5047271"))
+API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7896090354:AAE_NaVu_d-x-TCJt9CPNMl9t94Mltw_jrw")
 
 DOWNLOAD_PATH = os.path.join(os.getcwd(), "downloads")
 os.makedirs(DOWNLOAD_PATH, exist_ok=True)
